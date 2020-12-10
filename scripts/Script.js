34be4d453414c9576b9747200296df48136e7a80
Unknown <Ty@gimpel.ca>
2020-12-10 02:16:16 -0800

    Now adds images dynamically through JS.

--- scripts/Script.js
+++ scripts/Script.js
@@ -1,10 +1,20 @@
-function hideForm(){
-    // This function hides the form elements.
+function toggleForm(){
+    // This function toggles the form elements.
     let formChildren = document.getElementById('effectForm').children
     console.log(formChildren)
     for(var i = 0; i < formChildren.length; i++){
-        formChildren[i].toggleAttribute('hidden', true)
+        formChildren[i].toggleAttribute('hidden')
     }
 };
 
-hideForm();
+function addImgs(){
+    let thumbDiv = document.getElementById('thumb-bar')
+    for(var i = 1; i < 6; i++){
+        var img = document.createElement('img')
+        img.setAttribute("src", `images/pic${i}.jpg`)
+        thumbDiv.appendChild(img)
+    }
+}
+
+addImgs()
+toggleForm();
