0b81d1a687b93a21ce8ba76a2cbeb65c2f7dd40e
Unknown <Ty@gimpel.ca>
2020-12-10 02:49:30 -0800

    JS can now blur the displayed image.

--- scripts/Script.js
+++ scripts/Script.js
@@ -1,8 +1,9 @@
 let thumbDiv = document.getElementById('thumb-bar')
+
+
 function hideForm(){
     // This function toggles the form elements.
     let formChildren = document.getElementById('effectForm').children
-
     for(var i = 0; i < formChildren.length; i++){
         formChildren[i].toggleAttribute('hidden', true)
     }
@@ -23,6 +24,7 @@ function addImgs(){
         thumbDiv.appendChild(img)
     }
 }
+
 thumbDiv.addEventListener('mouseover', function(e){
     revealForm()
     var img = e.target.attributes[0].nodeValue
@@ -30,5 +32,20 @@ thumbDiv.addEventListener('mouseover', function(e){
     display.toggleAttribute('hidden', false)
     display.setAttribute('src', img)
 })
+
+let formButton = document.getElementsByTagName('button')[0]
+formButton.onclick = function(){
+    var usrInput = document.getElementById('effect').value
+    document.getElementById('effect').value = ""
+    var display = document.getElementsByClassName('displayed-img')[0]
+    if(usrInput.toLowerCase() === "blur"){
+        var img = display.attributes['src'].nodeValue
+        var imgBlur = img.slice(0, 11) + 'B' + img.slice(11)
+        display.setAttribute('src', imgBlur)
+    } else {
+        alert("Unrecognized Effect. Try 'Blur'")
+    }
+    return false //Must return false to ensure page does not refresh.
+}
 addImgs()
 hideForm();
