53885aa1737335d6449f1eb85246a6ecacf13186
Unknown <Ty@gimpel.ca>
2020-12-10 02:32:55 -0800

    JS can now dynamically add all images, and change the displayed image.

--- scripts/Script.js
+++ scripts/Script.js
@@ -1,20 +1,34 @@
-function toggleForm(){
+let thumbDiv = document.getElementById('thumb-bar')
+function hideForm(){
     // This function toggles the form elements.
     let formChildren = document.getElementById('effectForm').children
-    console.log(formChildren)
+
     for(var i = 0; i < formChildren.length; i++){
-        formChildren[i].toggleAttribute('hidden')
+        formChildren[i].toggleAttribute('hidden', true)
+    }
+};
+function revealForm(){
+    // This function toggles the form elements.
+    let formChildren = document.getElementById('effectForm').children
+    for(var i = 0; i < formChildren.length; i++){
+        formChildren[i].toggleAttribute('hidden', false)
     }
 };
 
 function addImgs(){
-    let thumbDiv = document.getElementById('thumb-bar')
+    
     for(var i = 1; i < 6; i++){
         var img = document.createElement('img')
         img.setAttribute("src", `images/pic${i}.jpg`)
         thumbDiv.appendChild(img)
     }
 }
-
+thumbDiv.addEventListener('mouseover', function(e){
+    revealForm()
+    var img = e.target.attributes[0].nodeValue
+    var display = document.getElementsByClassName('displayed-img')[0]
+    display.toggleAttribute('hidden', false)
+    display.setAttribute('src', img)
+})
 addImgs()
-toggleForm();
+hideForm();
